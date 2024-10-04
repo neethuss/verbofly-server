139753653b1b07528af7307302b4173e658920f1
neethuss <neethuanitha5@gmail.com>
2024-10-04 17:13:16 +0530

    deploy8

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: ["https://www.verbofly.life/", "https://verbofly.life/", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
   credentials: true
 }));
 
@@ -48,7 +49,7 @@ app.use('/lesson/', LessonRoutes);
 app.use('/chat/', ChatRoutes);
 app.use('/quiz/', QuizRoutes)
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.send('hai ')
 })
 const userSocketMap = new Map();
@@ -58,13 +59,13 @@ io.on('connection', (socket) => {
   socket.on('user_connected', (userId) => {
     // console.log('User connected:', userId);
     userSocketMap.set(userId, socket.id);
-    
+
   });
 
 
   socket.on('join chat', async (chatData) => {
     try {
-      const { chatId} = chatData;
+      const { chatId } = chatData;
       if (chatId) {
         socket.join(`chat_${chatId}`);
         // console.log(`User joined chat ${chatId}`);
@@ -79,10 +80,10 @@ io.on('connection', (socket) => {
   socket.on('chat message', async (messageData) => {
     try {
       // console.log('chat message', messageData);
-      const { chatId, senderId, receiverId,image,audio,call, messageText, createdAt } = messageData;
-      if (chatId && senderId && receiverId  && createdAt || messageText || image || audio) {
+      const { chatId, senderId, receiverId, image, audio, call, messageText, createdAt } = messageData;
+      if (chatId && senderId && receiverId && createdAt || messageText || image || audio) {
         const receiverSocketId = userSocketMap.get(receiverId);
-          io.to(receiverSocketId).emit('chat message', { chatId, senderId,image, messageText, audio, call ,createdAt});
+        io.to(receiverSocketId).emit('chat message', { chatId, senderId, image, messageText, audio, call, createdAt });
       } else {
         console.error("Message data is incomplete");
       }
@@ -91,19 +92,19 @@ io.on('connection', (socket) => {
     }
   });
 
-  
+
 
 
   socket.on('call', async (participants) => {
     console.log('call in backen')
     try {
-      const { caller, receiver} = participants;
-      console.log(participants,  'call');
-      if (participants) { 
+      const { caller, receiver } = participants;
+      console.log(participants, 'call');
+      if (participants) {
         console.log('incomingCall')
         const receiverSocketId = userSocketMap.get(receiver._id);
-        console.log(receiverSocketId,'sp')
-        io.to(receiverSocketId).emit('incomingCall', {caller,receiver });
+        console.log(receiverSocketId, 'sp')
+        io.to(receiverSocketId).emit('incomingCall', { caller, receiver });
       } else {
         console.error("Call data is incomplete");
       }
@@ -117,10 +118,10 @@ io.on('connection', (socket) => {
     try {
       console.log('Hangup during initiation event received:', ongoingCall);
       const { participants } = ongoingCall;
-      
+
       if (participants && participants.caller && participants.receiver) {
         const receiverSocketId = userSocketMap.get(participants.caller._id);
-        
+
         if (receiverSocketId) {
           io.to(receiverSocketId).emit('callCancelled', { message: 'The caller has cancelled the call.' });
         }
@@ -134,16 +135,16 @@ io.on('connection', (socket) => {
   });
 
   socket.on('webrtcSignal', async (data) => {
-    console.log(data,'webrtcSignal data')
-    if(data.isCaller){
-      if(data.ongoingCall.participants.receiver._id){
+    console.log(data, 'webrtcSignal data')
+    if (data.isCaller) {
+      if (data.ongoingCall.participants.receiver._id) {
         const emitSocketId = userSocketMap.get(data.ongoingCall.participants.receiver._id);
-        io.to(emitSocketId).emit('webrtcSignal',data)
+        io.to(emitSocketId).emit('webrtcSignal', data)
       }
-    }else{
-      if(data.ongoingCall.participants.caller._id){
+    } else {
+      if (data.ongoingCall.participants.caller._id) {
         const emitSocketId = userSocketMap.get(data.ongoingCall.participants.caller._id);
-        io.to(emitSocketId).emit('webrtcSignal',data)
+        io.to(emitSocketId).emit('webrtcSignal', data)
       }
     }
   });
@@ -152,14 +153,14 @@ io.on('connection', (socket) => {
     try {
       console.log('Hangup event received:', ongoingCall);
       const { participants } = ongoingCall;
-      
+
       if (participants && participants.caller && participants.receiver) {
-        const otherParticipantId = socket.id === userSocketMap.get(participants.caller._id) 
-          ? participants.receiver._id 
+        const otherParticipantId = socket.id === userSocketMap.get(participants.caller._id)
+          ? participants.receiver._id
           : participants.caller._id;
-        
+
         const otherParticipantSocketId = userSocketMap.get(otherParticipantId);
-        
+
         if (otherParticipantSocketId) {
           io.to(otherParticipantSocketId).emit('callEnded', { message: 'The other participant has ended the call.' });
         }
