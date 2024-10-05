718938079d4364b7712d3c9812d6f6b178355682
neethuss <neethuanitha5@gmail.com>
2024-10-05 16:14:02 +0530

    size

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,13 @@ const io = new Server(server, {
 
 
 app.use(cors({
-  origin: ["https://api.verbofly.life/lesson/addLesson", "https://www.verbofly.life", "https://verbofly.life", "http://localhost:3000"],
+  origin: ["https://www.verbofly.life", "https://verbofly.life", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
   credentials: true
 }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 app.use(bodyParser.json());
